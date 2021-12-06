e2c1089cf5703f507d58cc913c7bcaf00c229fe0
Jeff Chen <hello@jeff.yt>
2021-12-06 06:39:09 -0600

    day 6

---
+++ Day6.cs
@@ -0,0 +1,43 @@
+namespace aoc2021;
+
+public class Day6
+{
+    private static List<int> ReadInput()
+    {
+        return File.ReadAllText("inputs/day6.txt").Split(",").Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList();
+    }
+
+    private static ulong LanternFishReproduction(int days)
+    {
+        var fish = new ulong[9];
+        foreach (var input in ReadInput())
+        {
+            fish[input]++;
+        }
+
+        for (var day = 0; day < days; day++)
+        {
+            var babyFish = fish[0];
+
+            for (var i = 0; i < 8; i++)
+            {
+                fish[i] = fish[i + 1];
+            }
+
+            fish[8] = babyFish;
+            fish[6] += babyFish;
+        }
+
+        return fish.ToList().Aggregate((x, y) => x + y);
+    }
+
+    public static void Part1()
+    {
+        Console.WriteLine(LanternFishReproduction(80));
+    }
+
+    public static void Part2()
+    {
+        Console.WriteLine(LanternFishReproduction(256));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿using aoc2021;
 
-Day5.Part2();
+Day6.Part2();
